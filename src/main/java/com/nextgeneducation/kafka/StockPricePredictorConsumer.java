99e6e5833f6fcf273978bb50504620b24a734efd
Calvin Vette <calvinvette@gmail.com>
2019-12-17 12:27:43 -0500

    Fixed Group ID

--- src/main/java/com/nextgeneducation/kafka/StockPricePredictorConsumer.java
+++ src/main/java/com/nextgeneducation/kafka/StockPricePredictorConsumer.java
@@ -17,7 +17,7 @@ public class StockPricePredictorConsumer {
     public static void main( String[] args ) throws Exception {
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:9092");
-        props.setProperty("group.id", "test");
+        props.setProperty("group.id", "StockPricePredictorConsumer");
         props.setProperty("enable.auto.commit", "true");
         props.setProperty("auto.commit.interval.ms", "1000");
         props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
